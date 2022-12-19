3dfb0bdd9bf9d1a0a1931a954b5216614770ebc5
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-19 12:51:25 -0300

    feat: install and implements fonts and theme with styled-components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { StatusBar } from 'react-native';
+
+import { ThemeProvider } from 'styled-components'
+
+import theme from './src/theme'
+
+import {  useFonts, NunitoSans_400Regular, NunitoSans_700Bold} from '@expo-google-fonts/nunito-sans'
+
+
 export default function App() {
+
+  const [ fontsLoaded ] = useFonts( { NunitoSans_400Regular, NunitoSans_700Bold})
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle={'light-content'}
+        backgroundColor='transparent'
+        translucent
+      />
+      {
+        fontsLoaded 
+          ? <View style={styles.container}>
+              <Text>Open up App.tsx to start working on your app!</Text>
+            </View>
+          : null
+      }
+     
+    </ThemeProvider>
   );
 }
 
@@ -16,5 +38,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
