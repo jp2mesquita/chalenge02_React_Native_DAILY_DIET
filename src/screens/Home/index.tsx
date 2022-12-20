1dd9a3f4af35fb9e41bfbf3c8305814e4c921ec3
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-20 15:27:51 -0300

    feat: implement static sectionList on home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,127 @@
+import { Button } from '@components/Button'
+import { HomeHeader } from '@components/HomeHeader'
 import { useNavigation } from '@react-navigation/native'
-import { Button, Container, Text } from './styles'
+import { SectionList } from 'react-native'
+import { Container, List, ListTitle, MealCard, MealDietStatus, MealHour, MealName, PercentualText, StatisticsCard, Text, Title } from './styles'
+
+type Item = {
+  name: string,
+  description: string,
+  hour: string,
+  dietControl: 'INSIDE' | 'OUTSIDE'
+
+}
+
+interface DataProps {
+  date: string,
+  data: Item[]
+}
+
 
 export function Home(){
 
   const { navigate } = useNavigation()
-
+  
+  const mealRecordData: DataProps[]= [
+    {
+      date: '12.08.22',
+      data: [
+        {
+          name: 'Vitamina de Banana',
+          description: 'Vitamina de banana com morango',
+          hour: '09:30',
+          dietControl: 'INSIDE'
+        },
+        {
+          name: 'X-TUDO',
+          description: 'Xis completo da lancheria do bairro',
+          hour: '16:30',
+          dietControl: 'OUTSIDE'
+        },
+      ]
+    },
+    {
+      date: '11.08.22',
+      data: [
+        {
+          name: 'Vitamina de abacate',
+          description: 'Vitamina de abacate com morango',
+          hour: '09:30',
+          dietControl: 'INSIDE'
+        },
+        {
+          name: 'Almoço fitness',
+          description: 'Frango Grelhado e Arror',
+          hour: '16:30',
+          dietControl: 'INSIDE'
+        },
+        {
+          name: 'Shake poś-treino',
+          description: 'whey protein, banana, aveia, leite',
+          hour: '20:30',
+          dietControl: 'INSIDE'
+        },
+      ]
+    }
+  ]
+   
   return (
     <Container>
-      <Text>
-        Home
-      </Text>
-      <Button
+
+      <HomeHeader />
+
+      <StatisticsCard
         onPress={() => navigate('statistics')}
       >
+        <PercentualText>
+          90,86%
+        </PercentualText>
         <Text>
-          Statistics
+          das refeições dentro da dieta
         </Text>
-      </Button>
+      </StatisticsCard>
+
+      <Title>
+        Refeições
+      </Title>
+
+      <Button 
+        title='Nova Refeição'
+      />
+
+      <SectionList
+        showsVerticalScrollIndicator={false}
+        sections={mealRecordData as DataProps[]}
+        keyExtractor={(item) => item.name}
+        renderSectionHeader= { ({section: { date }}) => {
+
+          return(
+            <ListTitle>
+              {date}  
+            </ListTitle>
+          ) }
+        }
+        renderItem={ ({ item }) => {
+          const hour = item.hour
+          const name = item.name
+          const dietControl = item.dietControl
+
+        return(
+          <MealCard>
+            <MealHour>
+              {hour}
+            </MealHour>
+            <MealName>
+              {name}
+            </MealName>
+            <MealDietStatus 
+              status={dietControl}
+            />
+          </MealCard>
+          
+        )}}
+      />
+
     </Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,22 +1,116 @@
 import theme from "@theme/index";
+import { SectionList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
+  
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700} ;
+
+  padding: 24px;
+`
+
+export const StatisticsCard =  styled.TouchableOpacity`
+  width: 100%;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  padding-inline: 16px;
+
+  margin-top: 32px;
+
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_700} ;
+  
+  ${({ theme }) => css`
+    background-color: ${theme.COLORS.GREEN_LIGHT};
+  `}
+  border: none;
+  border-radius: 6px;
+`
+
+export const PercentualText = styled.Text`
+  ${({ theme }) => css`
+    color: ${ theme.COLORS.GRAY_100};
+    font-family: ${ theme.FONT_FAMILY.BOLD};
+    font-size: ${ theme.FONT_SIZE.XXL}px;
+  `}
 `
 
 export const Text = styled.Text`
-  color: ${({ theme }) => theme.COLORS.GRAY_100};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  ${({ theme }) => css`
+    color: ${ theme.COLORS.GRAY_100};
+    font-family: ${ theme.FONT_FAMILY.REGULAR};
+    font-size: ${ theme.FONT_SIZE.SM}px;
+  `}
 `
-export const Button =  styled.TouchableOpacity`
-  border: 1px solid ${({ theme }) => theme.COLORS.GRAY_300};
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    color: ${ theme.COLORS.GRAY_100};
+    font-family: ${ theme.FONT_FAMILY.REGULAR};
+    font-size: ${ theme.FONT_SIZE.MD}px;
+  `}
+
+  margin-top: 40px;
+  margin-bottom: 8px;
+`
+
+export const List = styled(SectionList)`
+  width: 100%;
+`
+
+export const ListTitle = styled.Text`
+  ${({ theme }) => css`
+    color: ${ theme.COLORS.GRAY_100};
+    font-family: ${ theme.FONT_FAMILY.BOLD};
+    font-size: ${ theme.FONT_SIZE.LG}px;
+  `}
+
+  margin-top: 32px;
+  margin-bottom: 12px;
+`
+
+export const MealCard = styled.View`
+  width: 100%;
+  flex-direction: row;
+
+  align-items: center;
+
+  gap: 2px;
+
+  border: 1px solid ${({ theme }) => theme.COLORS.GRAY_500};
   border-radius: 6px;
-  padding: 4px;
-  background-color: ${({ theme }) => theme.COLORS.GREEN_LIGHT};
+
+  margin-bottom: 8px;
+
+  padding: 12px;
+  padding-top: 16.5px;
+  padding-bottom: 16.5px;
+`
+export const MealHour = styled.Text`
+  padding-right: 12px;
+`
+
+export const MealName = styled.Text`
+  flex: 1;
+  padding-left: 12px;
+  border-left-width: 1px;
+  border-left-color: ${({ theme }) => theme.COLORS.GRAY_400};
+`
+export type MealDietStatusStyleProps = 'INSIDE' | 'OUTSIDE'
+
+interface MealDietStatusProps {
+  status?: MealDietStatusStyleProps
+}
+
+export const MealDietStatus = styled.View<MealDietStatusProps>`
+  width: 14px;
+  height: 14px;
+
+  border-radius: 99999px;
+  background-color: ${({ theme, status = "INSIDE" }) => 
+    status === 'INSIDE'
+      ? theme.COLORS.GREEN_LIGHT
+      : theme.COLORS.RED_LIGHT
+  }
 `
