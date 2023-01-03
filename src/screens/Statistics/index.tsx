b1d3727d7889794fe9064e49182e222a1cbe2259
João Paulo Mesquita <jp-mesquita@live.com>
2023-01-03 10:07:38 -0300

    feat: implements statistic page

--- src/screens/Statistics/index.tsx
+++ src/screens/Statistics/index.tsx
@@ -1,11 +1,85 @@
-import { Container, Text } from './styles'
+import { useNavigation, useRoute } from '@react-navigation/native'
+
+import { ColoredStatisticCard } from '@components/ColoredStatisticCard'
+import { RegularStatisticCard } from '@components/RegularStatisticCard'
+
+import { Text } from 'react-native'
+
+import { BackButton, BackIcon, ColoredCardsContainer, Container, Content, Header, Percent, Separator, Title } from './styles'
+import { useTheme } from 'styled-components/native'
+
+
+type RouteParams = {
+  isSuccessAboveFifity: boolean
+}
 
 export function Statistics(){
+
+  const { navigate } = useNavigation()
+
+  const route = useRoute()
+  const { isSuccessAboveFifity } = route.params as RouteParams
+
+  const { COLORS } = useTheme()
+  
+  function handleGoBack(){
+    navigate('home')
+  }
+
   return (
-    <Container>
-      <Text>
-          Statistics
-      </Text> 
+    <Container dietSuccessRate={isSuccessAboveFifity}>
+      <Header>
+        <BackButton
+          onPress={handleGoBack}
+        >
+          <BackIcon 
+            color= { isSuccessAboveFifity
+              ? COLORS.GREEN_DARK
+              : COLORS.RED_DARK
+            }
+          />
+        </BackButton>
+
+        <Percent>
+          90,86%
+        </Percent>
+        <Text>
+          das refeições dentro da dieta
+        </Text>
+      </Header>
+
+      <Content>
+        <Title>
+          Estatísticas gerais
+        </Title>
+
+        <RegularStatisticCard
+          title='22'
+          text='melhor sequencia de pratos dentro da dieta'
+        />
+
+        <RegularStatisticCard
+          title='109'
+          text='refeições registradas'
+        />
+
+        <ColoredCardsContainer>
+          <ColoredStatisticCard
+            title='99' 
+            text='refeições dentro da dieta'
+            type='INSIDE'
+          />
+
+          <Separator />
+
+          <ColoredStatisticCard
+            title='10' 
+            text='refeições fora da dieta'
+            type='OUTSIDE'
+          />
+        </ColoredCardsContainer>
+
+      </Content>
     </Container>
   )
 }
--- src/screens/Statistics/styles.ts
+++ src/screens/Statistics/styles.ts
@@ -1,16 +1,82 @@
+import { ArrowLeft } from "phosphor-react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
-export const Container = styled(SafeAreaView)`
+interface StatisticsProps{
+  dietSuccessRate: boolean
+}
+
+export const Container = styled(SafeAreaView)<StatisticsProps>`
   flex: 1;
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_700} ;
+  background-color: ${({ theme, dietSuccessRate}) =>
+    dietSuccessRate
+      ? theme.COLORS.GREEN_LIGHT
+      : theme.COLORS.RED_LIGHT
+  } ;
 
-  padding: 24px;
+  padding-top: 24px;
+`
+export const Header = styled.View`
+  
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 34px;
+  width: 100%;
+`
+
+export const BackButton = styled.TouchableOpacity`
+  flex: 1;
+  position: absolute;
+  left: 24px;
+  top: 0px;
+`
+
+export const BackIcon = styled(ArrowLeft).attrs(({ theme }) => ({
+  size: 24,
+}))
+``;
+
+export const Percent = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_100};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.XXL}px;
+  `
+  }
+  
+`
+
+export const Content = styled.View`
+  flex: 1;
+  width: 100%;
+  padding-left: 24px;
+  padding-right: 24px;
+  align-items: center;
+
+  border-radius: 20px;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
 
 `
 
-export const Text = styled.Text`
-  color: ${({ theme }) => theme.COLORS.GRAY_100};
+export const Title = styled.Text`
+  ${({theme}) => css`
+    color: ${theme.COLORS.GRAY_100};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.SM}px;;
+  `};
+
+  margin-top: 33px;
+  margin-bottom: 23px;
+
+`
+
+export const ColoredCardsContainer = styled.View`
+  width: 100%;
+  flex-direction: row;
+`
+export const Separator = styled.View`
+  width: 12px;
 `
