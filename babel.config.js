0a0c6ef5f38cbfc0e1ce8b836fb147cf78e52b10
João Paulo Mesquita <jp-mesquita@live.com>
2023-01-05 08:58:46 -0300

    chore: add utils path to paths

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = function(api) {
             '@routes': './src/routes',
             '@storage': './src/storage',
             '@theme': './src/theme',
+            '@utils': './src/utils'
           }
         }
       ]
