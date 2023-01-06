dc1ed7ab725f056160c2b289695248499a1cfd58
João Paulo Mesquita <jp-mesquita@live.com>
2023-01-06 11:39:35 -0300

    feat: create a type to fit params data

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,8 +1,13 @@
-type MealDetailsProps = {
+export type MealDetailsProps = {
   name: string,
   description: string,
   hour: string,
   dietControl: 'INSIDE' | 'OUTSIDE'
+}
+
+type MealToRegisterProps = {
+  date: string,
+  data: MealDetailsProps
 }
 
 export declare global {
@@ -18,7 +23,7 @@ export declare global {
       }
       newMeal: undefined
       registrationSuccess: {
-        dietControl: 'INSIDE' | 'OUTSIDE'
+        mealToRegister: MealToRegisterProps
       }
     }
   }
